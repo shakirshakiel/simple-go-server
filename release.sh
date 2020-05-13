f05a8c7b276e61eafbefa6446ba5e602dfdb8a08
Shakir Shakiel <shakirshakiel@gmail.com>
2020-05-13 16:01:14 +0530

    Incremenet version

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ set -e
 
 GOCD_URL=${GOCD_URL:-gocd.example.com}
 PROJECT_NAME=simple-go-server
-export VERSION=${VERSION:-1.0}
+export VERSION=${VERSION:-1.1}
 export BRANCH_NAME="release-$VERSION"
 
 # Update version in the template
